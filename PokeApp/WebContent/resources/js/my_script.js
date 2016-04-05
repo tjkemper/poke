e38c9e010a46021913f15359092942ab151bb6de
Taylor Kemper <taylor@revature.com>
2016-04-04 23:05:00 -0400

    set maxNumPokemon to 151 for red-blue. Made more reusable fn.

--- PokeApp/WebContent/resources/js/my_script.js
+++ PokeApp/WebContent/resources/js/my_script.js
@@ -1,74 +1,106 @@
 /**
  * Send AJAX request to the RESTful Pokemon API
+ * 
+ * @author Taylor Kemper
  */
 
 /*
- * Only run Javascript when page has loaded
+ * Set event handler on button
  */
 window.onload = function() {
-	document.getElementById("pokemonSubmit").addEventListener("click",
-			getPokemon);
+	document.getElementById("pokemonSubmit").addEventListener("click", getPokemon);
 }
+
+/*
+ * Max is 721, but red-blue has 151 pokemon
+ */
+var maxNumPokemon = 151;
+
+var pokemonName = "pokemon"; // global, name of pokemon
+var opponentName = "opponent";// global, name of opponent
+
+var pokemonJsonData; //global, JSON for pokemon
+var opponentJsonData;//global, JSON for opponent
+
+var pokemonDataSet = false;  //True iff pokemon data up to date
+var opponentDataSet = false; //True iff opponent data up to date
 
 function getPokemon() {
+	pokemonDataSet = false;
+	opponentDataSet = false;
+	
 	var base_url = "http://pokeapi.co/api/v2/pokemon/";
 
-	var pokemonId = document.getElementById("pokemonId").value;
+	var pokemonId = document.getElementById("selectedPokemonId").value;
+	
+	sendRequest(base_url + pokemonId, pokemonName);
 
-	sendRequest(base_url + pokemonId, 1);
-	
-	var randomOpponentId = Math.floor(Math.random() * 721) + 1;
-	
-	sendRequest(base_url + randomOpponentId, 2);
-	
+
+	var randomOpponentId = Math.floor(Math.random() * maxNumPokemon) + 1;
+
+	sendRequest(base_url + randomOpponentId, opponentName);
+
 }
 
-function sendRequest(url, num) {
+function sendRequest(url, name) {
 	var xhttp = new XMLHttpRequest();
 	xhttp.onreadystatechange = function() {
 		if (xhttp.readyState == 4 && xhttp.status == 200) {
-			var pokemon = JSON.parse(xhttp.responseText);
-			setValues(pokemon, num);
+			var jsonData = JSON.parse(xhttp.responseText);
+		
+			if(name === pokemonName){
+				pokemonJsonData = jsonData;
+			}else if(name === opponentName){
+				opponentJsonData = jsonData;
+			}
+			
+			setValuesBoth();
+			
 		}
 	};
 	xhttp.open("GET", url, true);
 	xhttp.send();
 }
 
-function setValues(pokemon, num) {
-	if (num == 1) {
-		document.getElementById("pokemonName").innerHTML = pokemon.name;
-		var pokemonImgElement = document.getElementById("pokemonImg");
-		pokemonImgElement.setAttribute("src", pokemon.sprites.front_default);
-		pokemonImgElement.setAttribute("alt", pokemon.name);
-		
-		var pokemonMovesElement = document.getElementById("pokemonMoves");
+function setValuesBoth(){
+	if(!pokemonDataSet){
+		setValues(pokemonJsonData, pokemonName);
+		pokemonDataSet = true;
+	}
 	
-		var move = document.createElement("li");
-		move.appendChild(document.createTextNode("punch"));
-		pokemonMovesElement.appendChild(move);
-		
-		var movesArray = pokemon.moves;
-		console.log(movesArray.length);
-		for(var i = 0; i < movesArray.length; i++){
-			var versionGroupDetailsArray = movesArray[i].version_group_details;
-			var include = false;
-			for(var j = 0; j < versionGroupDetailsArray.length; j++){
-				if(versionGroupDetailsArray[j].version_group.name === "red-blue"
-					&& versionGroupDetailsArray[j].level_learned_at === 0){
-					include = true;
-				}
-			}
-			if(include){
-				console.log(movesArray[i].move.name);
+	if(!opponentDataSet){
+		setValues(opponentJsonData, opponentName);
+		opponentDataSet = true;
+	}
+}
+
+function setValues(pokeJsonData, name) {
+
+	document.getElementById(name + "Name").innerHTML = pokeJsonData.name;
+	var imgElement = document.getElementById(name + "Img");
+	imgElement.setAttribute("src", pokeJsonData.sprites.front_default);
+	imgElement.setAttribute("alt", pokeJsonData.name);
+
+	var movesElement = document.getElementById(name + "Moves");
+	movesElement.innerHTML = ""; //reset so it doesn't add on to UL
+
+	var movesArray = pokeJsonData.moves;
+	console.log(movesArray.length);
+	for (var i = 0; i < movesArray.length; i++) {
+		var versionGroupDetailsArray = movesArray[i].version_group_details;
+		var include = false;
+		for (var j = 0; j < versionGroupDetailsArray.length; j++) {
+			if (versionGroupDetailsArray[j].version_group.name === "red-blue"
+					&& versionGroupDetailsArray[j].level_learned_at === 0) {
+				include = true;
 			}
 		}
-		
-		
-	}else if(num == 2){
-		document.getElementById("opponentName").innerHTML = pokemon.name;
-		var pokemonImgElement = document.getElementById("opponentImg");
-		pokemonImgElement.setAttribute("src", pokemon.sprites.front_default);
-		pokemonImgElement.setAttribute("alt", pokemon.name);		
+		if (include) {
+			var move = document.createElement("li");
+			move.appendChild(document.createTextNode(movesArray[i].move.name));
+			movesElement.appendChild(move);
+
+		}
 	}
+
 }
